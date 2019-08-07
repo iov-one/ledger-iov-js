0f1808e2d68c5e8a20f599d997e06f722d8f17d5
Juan Leni <lenijuan@gmail.com>
2019-08-07 17:02:35 +0200

    Initial commit
    common zx app example structure/template
    basic APDU functionality

---
+++ README.md
@@ -0,0 +1,7 @@
+# ledger-iov-js
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+[![npm version](https://badge.fury.io/js/ledger-iov.svg)](https://badge.fury.io/js/ledger-iov)
+[![CircleCI](https://circleci.com/gh/ZondaX/ledger-iov-js/tree/master.svg?style=shield)](https://circleci.com/gh/ZondaX/ledger-iov-js/tree/master)
+
+This package provides a basic client library to communicate with the IOV App running in a Ledger Nano S/X device
---
+++ tests/basic.ispec.js
@@ -0,0 +1,99 @@
+import LedgerApp from 'index.js';
+import TransportNodeHid from '@ledgerhq/hw-transport-node-hid';
+import { expect, test } from 'jest'
+import { Ed25519, Sha512 } from "@iov/crypto";
+import tou8 from "buffer-to-uint8array"
+
+test('get version', async () => {
+    const transport = await TransportNodeHid.create(1000);
+
+    const app = new LedgerApp(transport);
+    const version = await app.getVersion();
+    console.log(version);
+});
+
+test('get address', async () => {
+    const transport = await TransportNodeHid.create(1000);
+
+    const app = new LedgerApp(transport);
+
+    const pathAccount = 0x80000000;
+    const pathChange = 0x80000000;
+    const pathIndex = 0x80000005;
+
+    const response = await app.getAddress(pathAccount, pathChange, pathIndex);
+    console.log(response);
+
+    expect(response.pubKey).toEqual('6ed1781188602d62e9f38f4fb7eb3f51537d88f4589fcead933021d1a8867b05');
+    expect(response.address).toEqual('iov1dmghsyvgvqkk960n3a8m06el29fhmz85tz0uatvnxqsar2yx0vzw337ml');
+});
+
+test('show address', async () => {
+    jest.setTimeout(60000);
+
+    const transport = await TransportNodeHid.create(1000);
+
+    const app = new LedgerApp(transport);
+
+    const pathAccount = 0x80000000;
+    const pathChange = 0x80000000;
+    const pathIndex = 0x8000000A;
+    const response = await app.getAddress(pathAccount, pathChange, pathIndex, true);
+
+    console.log(response);
+
+    // FIXME: Address
+    expect(response.pubKey).toEqual('61d631e7dc190cdf62395ad44ac4495324178ee3975f37f03c523026d952f713');
+    expect(response.address).toEqual('iov1v8trre7uryxd7c3ett2y43zf2vjp0rhrja0n0upu2gczdk2j7uf9xtkyr');
+});
+
+test('sign1', async () => {
+    jest.setTimeout(60000);
+
+    const transport = await TransportNodeHid.create(1000);
+
+    // FIXME: Convert this to a proper pb serialized tx
+    const txBlobStr = '0102030405060708091011';
+
+    const txBlob = Buffer.from(txBlobStr, 'hex');
+
+    const app = new LedgerApp(transport);
+
+    const pathAccount = 0x80000000;
+    const pathChange = 0x80000000;
+    const pathIndex = 0x80000000;
+    const response = await app.sign(pathAccount, pathChange, pathIndex, txBlob);
+
+    console.log(response);
+});
+
+test('sign2_and_verify', async () => {
+    jest.setTimeout(60000);
+
+    const transport = await TransportNodeHid.create(1000);
+
+    // FIXME: Convert this to a proper pb serialized tx
+    const txBlobStr = '0102030405060708091011';
+
+    const txBlob = Buffer.from(txBlobStr, 'hex');
+
+    const app = new LedgerApp(transport);
+    const pathAccount = 0x80000000;
+    const pathChange = 0x80000000;
+    const pathIndex = 0x80000000;
+
+    const responseAddr = await app.getAddress(pathAccount, pathChange, pathIndex);
+    const responseSign = await app.sign(pathAccount, pathChange, pathIndex, txBlob);
+
+    const pubkey = tou8(Buffer.from(responseAddr.pubKey, 'hex'));
+
+    console.log(responseAddr);
+    console.log(responseSign);
+
+    // Check signature is valid
+    const prehash = new Sha512(txBlob).digest();
+    const signature = tou8(responseSign.signature);
+
+    const valid = await Ed25519.verifySignature(signature, prehash, pubkey);
+    expect(valid).toEqual(true);
+});
