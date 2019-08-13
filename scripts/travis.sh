a6fb7470c1fc1976b7bc80fc51c10d4a0d908043
Simon Warta <simon@warta.it>
2019-08-13 21:38:49 +0200

    Add Travis config

---
+++ scripts/travis.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck > /dev/null && shellcheck "$0"
+
+function fold_start() {
+  export CURRENT_FOLD_NAME="$1"
+  travis_fold start "$CURRENT_FOLD_NAME"
+  travis_time_start
+}
+
+function fold_end() {
+  travis_time_finish
+  travis_fold end "$CURRENT_FOLD_NAME"
+}
+
+fold_start "yarn-install"
+yarn install
+fold_end
+
+fold_start "yarn-build"
+yarn build
+fold_end
+
+fold_start "yarn-test-unit"
+yarn test:unit
+fold_end
